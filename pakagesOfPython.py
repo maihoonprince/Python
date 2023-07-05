3cf2f0fa22199ce54f65dea6758a429215f27020
PR!nce <108923246+maihoonprince@users.noreply.github.com>
2023-07-05 14:48:52 +0530

    Add files via upload

---
+++ pakagesOfPython.py
@@ -0,0 +1,29 @@
+#pip and module is one of the reason for python popularity.
+
+
+# Python, being a versatile programming language, offers a vast ecosystem of packages and libraries that extend its functionality and enable developers to perform a wide range of tasks. Here are some popular Python packages across different domains:
+
+# NumPy: A fundamental package for scientific computing, providing support for large, multi-dimensional arrays and matrices, along with a collection of mathematical functions.
+
+# Pandas: A powerful data manipulation and analysis library that offers data structures and operations for efficient handling of structured data.
+
+# Matplotlib: A plotting library that provides a MATLAB-like interface for creating static, animated, and interactive visualizations in Python.
+
+# TensorFlow: An open-source machine learning framework designed for numerical computation and large-scale machine learning projects, with a focus on deep learning.
+
+# Scikit-learn: A machine learning library that provides a range of tools for data preprocessing, classification, regression, clustering, and more. It is built on top of NumPy, SciPy, and Matplotlib.
+
+# Keras: A user-friendly deep learning library that acts as an interface to TensorFlow, simplifying the process of building and training neural networks.
+
+# Flask: A lightweight web framework that allows developers to build web applications quickly and easily. It provides features for URL routing, request handling, templating, and more.
+# Django: A high-level web framework that follows the model-view-controller (MVC) architectural pattern. It provides a complete set of tools for building robust and scalable web applications.
+
+# Requests: A simple and elegant HTTP library that makes it easy to send HTTP requests and handle responses in Python.
+
+# Beautiful Soup: A library for web scraping that helps in parsing HTML and XML documents, extracting data, and navigating the parsed tree structure.
+
+# SQLAlchemy: A SQL toolkit and Object-Relational Mapping (ORM) library that provides a set of high-level API for interacting with databases, making it easier to work with relational databases in Python.
+
+# Pygame: A cross-platform library for game development that provides functionality for graphics, sound, and input handling, allowing developers to create interactive games in Python.
+
+# These are just a few examples among the vast array of Python packages available. The Python Package Index (PyPI) is a comprehensive repository where you can explore and discover thousands of additional packages for various purposes.
