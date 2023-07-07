bd20df098e6d04ada09d2173323c24e7a061aeaf
PR!nce <108923246+maihoonprince@users.noreply.github.com>
2023-07-07 12:46:52 +0530

    Add files via upload

---
+++ forLoop.py
@@ -0,0 +1,17 @@
+# A for loop is a control flow statement in Python that allows you to iterate over a sequence of elements.
+# It is commonly used when you have a known number of iterations or when you want to perform a set of operations on each item in a collection.
+
+for i in range(5):
+    print(i+1)
+
+a = [1, 34, 456, 76, 459]
+for item in a:
+    print(item)
+
+print("Printing set......")
+s = {23, "prince", 34}
+for item in s:
+    print(item)
+
+#it is used to many itrable elements...
+
